39b99b24fd4b7aa3306f6c427f61b06ac53a8655
fayob <adedejifavour6@gmail.com>
2024-04-06 15:52:37 +0100

    Default

---
+++ src/default.rs
@@ -0,0 +1,23 @@
+#[derive(Debug)]
+struct Package {
+    weight: f64,
+}
+
+impl Package {
+     fn new(weight: f64) -> Self {
+        Self {
+            weight
+        }
+     }
+}
+
+impl Default for Package {
+    fn default() -> Self {
+        Self { weight: 4.0 }
+    }
+}
+
+fn main(){
+    let p = Package::default();
+    println!("{:?}", p);
+}
