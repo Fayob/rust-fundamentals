1d3d2d12e3669d83789580cb8ace5f823fc4b480
fayob <adedejifavour6@gmail.com>
2024-04-21 18:00:17 +0100

    External Module

---
+++ src/external_module.rs
@@ -0,0 +1,9 @@
+use activity::msg::*;
+
+fn main() {
+    activity::math::divide(4, 2);
+    
+    lowercase_word("LOWERCASE WORD");
+    trim_word("trim           words");
+    uppercase_word("upppercase word");
+}
