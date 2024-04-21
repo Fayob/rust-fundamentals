0d6cf021a7914a14708b55da92b814d74a401383
fayob <adedejifavour6@gmail.com>
2024-04-21 18:04:57 +0100

    Add msg file

---
+++ src/dir/msg.rs
@@ -0,0 +1,11 @@
+pub fn trim_word(msg: &str) {
+    println!("{:?}", msg.trim())
+}
+
+pub fn lowercase_word(msg: &str) {
+    println!("The length of the message is =>{:?}", msg.to_lowercase())
+}
+
+pub fn uppercase_word(msg: &str) {
+    println!("{:?}", msg.to_uppercase())
+}
