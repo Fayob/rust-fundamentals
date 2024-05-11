cdd8fd04d0b0caf65d6a7a6590620c1ef976002e
fayob <adedejifavour6@gmail.com>
2024-05-11 15:40:04 +0100

    Implementing Iterator

---
+++ src/implementing_iterator.rs
@@ -0,0 +1,36 @@
+struct Odd {
+    number: isize,
+    max: isize,
+}
+
+impl Odd {
+    fn new(max: isize) -> Self {
+        Self { number: -1, max }
+    }
+}
+
+impl Iterator for Odd {
+    type Item = isize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.number += 2;
+        if self.number <= self.max {
+            Some(self.number)
+        } else {
+            None
+        }
+    } 
+}
+fn main() {
+    let mut odds = Odd::new(7);
+    println!("{:?}", odds.next());
+    println!("{:?}", odds.next());
+    println!("{:?}", odds.next());
+    println!("{:?}", odds.next());
+    println!("{:?}", odds.next());
+
+    let evens = Odd::new(8);
+    for e in evens.map(|odd| odd + 1) {
+        println!("even: {}", e)
+    }
+}
